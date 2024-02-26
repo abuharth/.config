38639c0e3551ac27b2c26617499cfc5e837d8139
Ahmed Abuharthieh <145587343+abuharth@users.noreply.github.com>
2024-02-26 15:27:55 -0500

    Update remap.lua

--- nvim/lua/abuharth/remap.lua
+++ nvim/lua/abuharth/remap.lua
@@ -12,4 +12,10 @@ vim.keymap.set("n", "N", "Nzzzv")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
+-- select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- open terminal in vertical split
+vim.keymap.set("n", "<leader>t", ":vsplit<CR><C-w>w:term<CR>a")
+-- normal mode from terminal
 vim.keymap.set("t", "<C-q>", "<C-\\><C-n>")
